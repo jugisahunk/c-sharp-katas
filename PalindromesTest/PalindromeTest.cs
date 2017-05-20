b3764427f08cace2f581f64ef1d898020e616b05
Jason Knight <jugisahunk@gmail.com>
2017-05-20 14:48:16 -0500

    added remaining palindrome tests

--- PalindromesTest/PalindromeTest.cs
+++ PalindromesTest/PalindromeTest.cs
@@ -24,5 +24,57 @@ namespace Palindromes
 
             Assert.IsTrue(result);
         }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_empty_string(){
+            bool result = _testee.IsPalindrome(String.Empty);
+
+            Assert.IsTrue(result);
+        }
+        
+        [Test()]
+        public void IsPalindrome_returns_false_for_ABCDE(){
+            string testString = "ABCDE";
+
+            bool result = _testee.IsPalindrome(testString);
+
+            Assert.IsFalse(result);
+        }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_ABBA(){
+            string testString = "ABBA";
+
+            bool result = _testee.IsPalindrome(testString);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_racecar(){
+            string testString = "racecar";
+
+            bool result = _testee.IsPalindrome(testString);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_Bob(){
+            string testString = "Bob";
+
+            bool result = _testee.IsPalindrome(testString);
+
+            Assert.IsTrue(result);
+        }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_dammit_im_mad(){
+            string testString = "Dammit, I'm mad!";
+
+            bool result = _testee.IsPalindrome(testString);
+
+            Assert.IsTrue(result);
+        }
     }
 }
