f1da628f149743ae69dd6e75b768580908492278
Jason Knight <jugisahunk@gmail.com>
2017-05-20 14:20:23 -0500

    test basic setup for palindromes

---
+++ Palindromes/Palindrome.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Palindromes
+{
+    public class Palindrome
+    {
+        public Palindrome()
+        {
+		}
+
+		public bool IsPalindrome(string s)
+		{
+            throw new NotImplementedException();
+		}
+    }
+}
---
+++ PalindromesTest/PalindromeTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+namespace Palindromes
+{
+    [TestFixture()]
+    public class PalindromeTests
+    {
+        private Palindrome _testee;
+
+        [OneTimeSetUp]
+        public void SetUp(){
+            _testee = new Palindrome();
+        }
+
+        [OneTimeTearDown]
+        public void TearDown(){
+            _testee = null;
+        }
+
+        [Test()]
+        public void IsPalindrome_returns_true_for_null()
+        {
+            bool result = _testee.IsPalindrome(null);
+
+            Assert.IsTrue(result);
+        }
+    }
+}
