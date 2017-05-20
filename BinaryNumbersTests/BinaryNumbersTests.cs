4162e83afdb7cfbcf254fb122b85bddfba83d944
Jason Knight <jugisahunk@gmail.com>
2017-05-20 14:05:55 -0500

    removed 31 digit requirement for binary numbers

--- BinaryNumbersTests/BinaryNumbersTests.cs
+++ BinaryNumbersTests/BinaryNumbersTests.cs
@@ -23,35 +23,35 @@ namespace BinaryNumbers
 		public void BinaryNumbers_returns_00000000000000000000000000000000_for_input_0()
 		{
 			var result = binaryNumbers.IntegerToBinary(0);
-			Assert.AreEqual("0000000000000000000000000000000", result);
+			Assert.AreEqual("0", result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_00000000000000000000000000000001_for_input_1()
 		{
 			var result = binaryNumbers.IntegerToBinary(1);
-			Assert.AreEqual("0000000000000000000000000000001", result);
+			Assert.AreEqual("1", result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_0000000000000000000010000000000_for_input_1024()
 		{
 			var result = binaryNumbers.IntegerToBinary(1024);
-			Assert.AreEqual("0000000000000000000010000000000", result);
+			Assert.AreEqual("10000000000", result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_00000000000000001111111111111111_for_input_65535()
 		{
 			var result = binaryNumbers.IntegerToBinary(65535);
-			Assert.AreEqual("0000000000000001111111111111111", result);
+			Assert.AreEqual("1111111111111111", result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_0000000000100000000000000000000_for_input_1048576()
 		{
 			var result = binaryNumbers.IntegerToBinary(1048576);
-			Assert.AreEqual("0000000000100000000000000000000", result);
+			Assert.AreEqual("100000000000000000000", result);
 		}
 
 		[Test]
@@ -71,35 +71,35 @@ namespace BinaryNumbers
 		[Test]
 		public void BinaryNumbers_returns_0_for_input_00000000000000000000000000000000()
 		{
-			var result = binaryNumbers.BinaryToInteger("0000000000000000000000000000000");
+			var result = binaryNumbers.BinaryToInteger("0");
 			Assert.AreEqual(0, result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_1_for_input_00000000000000000000000000000001()
 		{
-			var result = binaryNumbers.BinaryToInteger("0000000000000000000000000000001");
+			var result = binaryNumbers.BinaryToInteger("1");
 			Assert.AreEqual(1, result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_1024_for_input_0000000000000000000010000000000()
 		{
-			var result = binaryNumbers.BinaryToInteger("0000000000000000000010000000000");
+			var result = binaryNumbers.BinaryToInteger("10000000000");
 			Assert.AreEqual(1024, result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_65535_for_input_00000000000000001111111111111111()
 		{
-			var result = binaryNumbers.BinaryToInteger("0000000000000001111111111111111");
+			var result = binaryNumbers.BinaryToInteger("1111111111111111");
 			Assert.AreEqual(65535, result);
 		}
 
 		[Test]
 		public void BinaryNumbers_returns_10485760_for_input_000000000010000000000000000000()
 		{
-			var result = binaryNumbers.BinaryToInteger("0000000000100000000000000000000");
+			var result = binaryNumbers.BinaryToInteger("100000000000000000000");
 			Assert.AreEqual(1048576, result);
 		}
 
@@ -113,7 +113,7 @@ namespace BinaryNumbers
 		[Test]
 		public void BinaryNumbers_returns_2147483647_for_input_01111111111111111111111111111111()
 		{
-			var result = binaryNumbers.BinaryToInteger("01111111111111111111111111111111");
+			var result = binaryNumbers.BinaryToInteger("1111111111111111111111111111111");
 			Assert.AreEqual(2147483647, result);
 		}
     }
