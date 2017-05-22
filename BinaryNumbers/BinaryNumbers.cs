6099d08cd4db9882902b9bcc3ddd15ad8d6a5281
Jason Knight <jugisahunk@gmail.com>
2017-05-22 12:26:02 -0500

    my solutions for katas

--- BinaryNumbers/BinaryNumbers.cs
+++ BinaryNumbers/BinaryNumbers.cs
@@ -1,17 +1,41 @@
 ﻿using System;
+using System.Collections;
+using System.Linq;
 
 namespace BinaryNumbers
 {
 	public class BinaryNumbers
 	{
+        IEnumerable _powersOfTwo;
+
+        public BinaryNumbers(){
+            _powersOfTwo = Enumerable.Range(0,31).Reverse();
+        }
 		public string IntegerToBinary(int i)
-		{
-			throw new NotImplementedException();
+        {
+            /*string binaryResult = String.Empty;
+            double remainder = i;
+
+            foreach(int power in _powersOfTwo){
+                double product = Math.Pow(2, power);
+
+                if(product <= remainder){
+                    binaryResult += "1";
+                    remainder -= product;
+                }
+                else{
+                    binaryResult += "0";
+                }
+            }
+
+            return binaryResult;*/
+
+            return Convert.ToString(i, 2);
 		}
 
 		public int BinaryToInteger(string s)
 		{
-			throw new NotImplementedException();
+            return Convert.ToInt32(s, 2);
 		}
 	}
 }
--- Palindromes/Palindrome.cs
+++ Palindromes/Palindrome.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text.RegularExpressions;
 namespace Palindromes
 {
     public class Palindrome
@@ -7,9 +8,25 @@ namespace Palindromes
         {
 	}
 
+<<<<<<< Updated upstream
 	public bool IsPalindrome(string s)
 	{
             throw new NotImplementedException();
 	}
+=======
+		public bool IsPalindrome(string s)
+		{
+            if (s is null || s == String.Empty)
+                return true;
+
+            string cleaned = Regex.Replace(s, "[^a-zA-Z]", String.Empty);
+            string lowered = cleaned.ToLower();
+            char[] chars = lowered.ToCharArray();
+            Array.Reverse(chars);
+            string reversedString = new string(chars);
+
+            return reversedString == lowered;
+		}
+>>>>>>> Stashed changes
     }
 }
