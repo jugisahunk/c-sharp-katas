17295ad8ed034d663508bb01c2a7da5c429f0f12
Jason Knight <jugisahunk@gmail.com>
2017-05-22 12:34:24 -0500

    removing my solutions to katas

--- BinaryNumbers/BinaryNumbers.cs
+++ BinaryNumbers/BinaryNumbers.cs
@@ -6,36 +6,17 @@ namespace BinaryNumbers
 {
 	public class BinaryNumbers
 	{
-        IEnumerable _powersOfTwo;
 
         public BinaryNumbers(){
-            _powersOfTwo = Enumerable.Range(0,31).Reverse();
         }
 		public string IntegerToBinary(int i)
         {
-            /*string binaryResult = String.Empty;
-            double remainder = i;
-
-            foreach(int power in _powersOfTwo){
-                double product = Math.Pow(2, power);
-
-                if(product <= remainder){
-                    binaryResult += "1";
-                    remainder -= product;
-                }
-                else{
-                    binaryResult += "0";
-                }
-            }
-
-            return binaryResult;*/
-
-            return Convert.ToString(i, 2);
+            throw new NotImplementedException();
 		}
 
 		public int BinaryToInteger(string s)
 		{
-            return Convert.ToInt32(s, 2);
+            throw new NotImplementedException();
 		}
 	}
 }
--- Palindromes/Palindrome.cs
+++ Palindromes/Palindrome.cs
@@ -6,27 +6,11 @@ namespace Palindromes
     {
         public Palindrome()
         {
-	}
+        }
 
-<<<<<<< Updated upstream
-	public bool IsPalindrome(string s)
-	{
+        public bool IsPalindrome(string s)
+        {
             throw new NotImplementedException();
-	}
-=======
-		public bool IsPalindrome(string s)
-		{
-            if (s is null || s == String.Empty)
-                return true;
-
-            string cleaned = Regex.Replace(s, "[^a-zA-Z]", String.Empty);
-            string lowered = cleaned.ToLower();
-            char[] chars = lowered.ToCharArray();
-            Array.Reverse(chars);
-            string reversedString = new string(chars);
-
-            return reversedString == lowered;
-		}
->>>>>>> Stashed changes
+        }
     }
 }
