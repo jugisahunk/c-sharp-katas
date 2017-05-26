fc5719b1f78a34f3633c2709fc979a940ad4c29a
Jason Knight <jugisahunk@gmail.com>
2017-05-26 13:35:49 -0500

    added default solution file for bowling kata

---
+++ BowlingGame/BowlingGame.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BowlingGame
+{
+    public class BowlingGame
+    {
+        public BowlingGame()
+        {
+        }
+
+		public void Roll(int pins)
+		{
+			throw new NotImplementedException();
+        }
+
+        public int Score()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
