2719f5c87d145eaeab25a35b4f0ecbf4485906b8
Jason Knight <jugisahunk@gmail.com>
2017-05-26 11:24:07 -0500

    added first test

---
+++ BowlingGameTests/BowlingGameTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System;
+namespace BowlingGame
+{
+
+    [TestFixture()]
+    public class BowlingGameTests
+	{
+		private BowlingGame _testee;
+
+		[OneTimeSetUp]
+		public void Setup()
+		{
+			_testee = new BowlingGame();
+		}
+
+        [OneTimeTearDown()]
+        public void Teardown(){
+            _testee = null;
+        }
+
+        [Test()]
+        public void Score_Gutter_Game(){
+            for (int i = 0; i < 20; i++){
+                _testee.Roll(0);
+            }
+
+            int expectedResult = 0;
+
+            int actualResult = _testee.Score();
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
