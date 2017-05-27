b2872c8ffafd0e59c10ecc02a1ac93ed5c340f3f
Jason Knight <jugisahunk@gmail.com>
2017-05-27 09:39:07 -0500

    resetting tests

--- BowlingGameTests/BowlingGameTests.cs
+++ BowlingGameTests/BowlingGameTests.cs
@@ -1,107 +1,31 @@
 ﻿using NUnit.Framework;
-using System;
-namespace BowlingGame
+
+[TestFixture()]
+public class BowlingGameTests
 {
+    [Test()]
+    public void Score_Gutter_Game()
+    {
+        Game g = new Game(); 
+    }
 
-    [TestFixture()]
-    public class BowlingGameTests
-	{
-		private BowlingGame _testee;
+    [Test(), Ignore("")]
+    public void Score_All_Ones()
+    {
+    }
 
-        [SetUp()]
-		public void Setup()
-		{
-			_testee = new BowlingGame();
-		}
+    [Test(), Ignore("")]
+    public void Score_One_Spare()
+    {
+    }
 
-        [TearDown()]
-        public void Teardown(){
-            _testee = null;
-        }
+    [Test(), Ignore("")]
+    public void Score_One_Strike()
+    {
+    }
 
-        private void RollMany(int rolls, int pins)
-        {
-			for (int i = 0; i < rolls; i++)
-			{
-				_testee.Roll(pins);
-			}
-        }
-
-        [Test()]
-        public void Score_Gutter_Game()
-        {
-            int rolls = 20;
-
-            RollMany(rolls, 0);
-
-            int expectedResult = 0;
-
-            int actualResult = _testee.Score();
-
-            Assert.AreEqual(expectedResult, actualResult);
-        }
-
-        [Test()]
-        public void Score_All_Ones()
-        {
-            int rolls = 20;
-
-            int expectedResult = 20;
-
-            RollMany(rolls, 1);
-
-            int actualResult = _testee.Score();
-
-            Assert.AreEqual(expectedResult,actualResult);
-        }
-
-        [Test()]
-        public void Score_One_Spare()
-        {
-            int rolls = 17;
-
-            RollSpare();
-            _testee.Roll(3);
-
-            RollMany(rolls, 0);
-
-            int actualResult = _testee.Score();
-
-            Assert.AreEqual(16, actualResult);
-        }
-
-        [Test()]
-        public void Score_One_Strike()
-        {
-            RollStrike();
-            _testee.Roll(3);
-            _testee.Roll(4);
-            RollMany(16, 0);
-
-            int actualResult = _testee.Score();
-
-            Assert.AreEqual(24, actualResult);
-        }
-
-        [Test()]
-        public void Score_Perfect_Game()
-        {
-            RollMany(12, 10);
-
-            int actualResult = _testee.Score();
-
-            Assert.AreEqual(300, actualResult);
-        }
-
-        private void RollStrike()
-        {
-            _testee.Roll(10);
-        }
-
-        private void RollSpare()
-        {
-            _testee.Roll(5);
-            _testee.Roll(5);
-        }
+    [Test(), Ignore("")]
+    public void Score_Perfect_Game()
+    {
     }
 }
