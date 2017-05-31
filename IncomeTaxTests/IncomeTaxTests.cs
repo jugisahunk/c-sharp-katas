021bd15130c48fec24ce25a866a03aac92d016dc
Jason Knight <jugisahunk@gmail.com>
2017-05-31 11:11:16 -0500

    adding income tax kata

---
+++ IncomeTaxTests/IncomeTaxTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture()]
+public class IncomeTaxTests
+{
+    [Test()]
+    public void No_Income_Resturns_0()
+    {
+      IncomeTax tax = new IncomeTax();
+    }
+}
