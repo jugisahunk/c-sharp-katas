920a748f25e72b9265235b4fbaa2378eaaa32029
Jason Knight <jugisahunk@gmail.com>
2017-06-02 19:05:48 -0500

    added start of anagram kata

---
+++ AnagramsTests/AnagramsTests.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+using System;
+
+[TestFixture()]
+public class AnagramsTest
+{
+    [Test()]
+    public void A_Returns_A()
+    {
+        AnagramChecker checker = new AnagramChecker();
+    }
+}
